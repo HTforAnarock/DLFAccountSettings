90cb2aadc1bf1f8880bb0f518117b85725dfa91e
Harsh.Tiwari <harsh.tiwari@anarock.com>
2023-06-21 11:00:20 +0530

    confirm Tick Added

--- AccountSettings/DeleteAccountViewController.swift
+++ AccountSettings/DeleteAccountViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 class DeleteAccountViewController: UIViewController {
 
+    @IBOutlet weak var confirmButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    
-    /*
-    // MARK: - Navigation
+    @IBAction func confirmButtonAction(_ sender: Any) {
+        let storyBoard = UIStoryboard(name: "ConfirmTickMarkViewController", bundle: nil)
+        let vc = storyBoard.instantiateViewController(withIdentifier: "ConfirmTickMarkViewController") as! ConfirmTickMarkViewController
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        self.present(vc, animated: true, completion: nil)
     }
-    */
+    
 
 }
